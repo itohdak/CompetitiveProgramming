da39f825ecc111294796aaf51e54d98e52b419d5
Hideaki Ito <itohdak@gmail.com>
2026-04-30 00:46:46 +0900

    solve ABC454F

--- AtCoder/ABC/454/F.cpp
+++ AtCoder/ABC/454/F.cpp
@@ -19,8 +19,33 @@ const ld eps = 1e-10;
 template<typename T1, typename T2> inline void chmin(T1 &a, T2 b){if(a>b) a=b;}
 template<typename T1, typename T2> inline void chmax(T1 &a, T2 b){if(a<b) a=b;}
 
+void solve() {
+  int n, m; cin >> n >> m;
+  vector<int> A(n); rep(i, n) cin >> A[i];
+  vector<int> B(n/2);
+  rep(i, n/2) {
+    B[i] = (A[i]-A[n-1-i]+m)%m;
+  }
+  vector<int> C(n/2+1);
+  rep(i, n/2+1) {
+    C[i] = ((i==n/2 ? 0 : B[i]) - (i==0 ? 0 : B[i-1]) + m) % m;
+  }
+  sort(all(C));
+  ll ans = longinf;
+  ll cum1 = 0, cum2 = 0;
+  rep(i, C.size()) cum2 += m-C[i];
+  ans = cum2;
+  rep(i, C.size()) {
+    cum1 += C[i];
+    cum2 -= m-C[i];
+    chmin(ans, max(cum1, cum2));
+  }
+  cout << ans << endk;
+}
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int t; cin >> t;
+  while(t--) solve();
   return 0;
 }
